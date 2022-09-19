787828f2c730414065e06bd76aaeebe297a14d9c
Joaquin Gimenez <js.gimenez98@gmail.com>
2022-09-18 23:21:49 -0300

    Desafio context

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,20 +1,26 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Cart from "./images/carrito.png"
+import trash from "./images/trash.png"
 import {CartContext} from './Context';
 
 
 function CartWidget () {
-    const {cartTotal, } = useContext(CartContext);
+    const {cartTotal, clear} = useContext(CartContext);
 
 
     return (
-        <Link to={"/cart"}>
-            <button type="button" className="btn position-relative" title="Ir al Carrito">
-                <img src={Cart} width="30" alt="Carrito" />
-                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{cartTotal()}</span>
+        <div>
+            <Link to={"/cart"}>
+                <button type="button" className="btn position-relative"     title="Ir al Carrito">
+                    <img src={Cart} width="30" alt="Carrito" />
+                    <span className="position-absolute top-0 start-100  translate-middle badge rounded-pill bg-danger">{cartTotal()} </span>
+                </button>
+            </Link>   
+            <button type="button" className="btn position-relative me-1" title="Vaciar Carrito" onClick={()=> {clear()}}>
+                <img src={trash} width="35" alt="Cesto" />
             </button>
-        </Link>
+        </div>
     )
 }
 
