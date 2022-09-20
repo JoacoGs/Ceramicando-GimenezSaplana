4a3485e3c108a87cc0068c925bd9a1ae9943e190
Joaquin Gimenez <js.gimenez98@gmail.com>
2022-09-20 17:20:22 -0300

    Desafio context

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,12 +1,15 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
 import ItemCount from './ItemCount';
 import {CartContext} from './Context';
 
 const ItemDetail = ({ item }) => {
     const {addItem} = useContext(CartContext);
+    const [counter, setCounter] = useState(0);
 
     const onAdd = (counter) => {
+        setCounter(counter);
         addItem(item, counter);
     }
 
@@ -20,7 +23,7 @@ const ItemDetail = ({ item }) => {
             <Card.Title className="text-center"><b>{nombre}</b></Card.Title>
             <Card.Text className="text-center"> ${precio}</Card.Text>
             </Card.Body> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem soluta ratione quod in fugit repudiandae inventore eveniet eius, cum voluptate esse sequi sapiente aut error at blanditiis sint minima!
-            <ItemCount stock={stock} initial={1} onAdd={onAdd}  />
+            {counter === 0 ? <ItemCount stock={stock} initial={1} onAdd={onAdd} /> : <Link to={"/cart"} className="btn">Ir al Carrito</Link>}
             </Card>
         </div>
     );
